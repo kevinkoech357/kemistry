6705792bd8d2d95e2dd535d434d2483ca820710a
Kevin Koech <kevinkoech@proton.me>
2024-04-04 13:58:11 +0300

    [Chore]: Delete tests

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from kemistry import create_app
-
-
-@pytest.fixture
-def app():
-    app = create_app()
-
-    return app
--- tests/test_views_status.py
+++
@@ -1,5 +0,0 @@
-from flask import url_for
-
-
-def test_app(client):
-    assert client.get(url_for("user1.contact")).status_code == 200
